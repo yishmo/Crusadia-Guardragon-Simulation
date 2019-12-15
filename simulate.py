c35432da39d683e4967169cb50a75ab6ba013084
Yishan McNabb <ymcnabb@uci.edu>
2019-12-15 15:30:19 -0800

    project is at turn-in stage

--- simulate.py
+++ simulate.py
@@ -8,23 +8,103 @@ import time
 import os
 from collections import defaultdict
 
-NUM_HANDS = 200 
-NUM_SAMPLE_MEANS = 201
-DECK_FILE_NAME = 'test.ygo'
-
-deck_dirs = {'one' : 8, 'two' : 8,'three' : 8,'four' : 8,'five' : 8,'six' : 8,
-             'seven' : 8,'eight' : 8,'nine' : 8,'ten' : 7,'eleven' : 5,'twelve' : 3,}
+NUM_HANDS = 100 
+NUM_SAMPLE_MEANS = 101
+DECK_FILE_NAME = 'CG.ygo'
 
 
 
 
 
-firstRoundWinners = [('./nine/nine5.ygo', 754),
-                    ('./ten/ten4.ygo', 685),
-                    ('./ten/ten2.ygo', 622),
-                    ('./eleven/eleven2.ygo', 581),
-                    ('./nine/nine3.ygo', 573),
-                    ('./eight/eight5.ygo', 567),]
+#this variable is the list you use to rank decks:
+ALL_DECK_NAMES = [('./ten/ten2.ygo', 5582),
+                ('./nine/nine5.ygo', 5544),
+                ('./eight/eight5.ygo', 5526),
+                ('./eleven/eleven2.ygo', 5463),
+                ('./nine/nine2.ygo', 5456),
+                ('./nine/nine1.ygo', 5372),
+                ('./seven/seven2.ygo', 5288),
+                ('./eight/eight1.ygo', 5224), 
+                ('./five/five5.ygo', 5133),
+                ('./eleven/eleven3.ygo', 5109), 
+                ('./ten/ten4.ygo', 5075),
+                ('./nine/nine3.ygo', 5067),
+                ('./six/six2.ygo', 5045),
+                ('./eight/eight2.ygo', 4991),
+                ('./ten/ten5.ygo', 4989),
+                ('./ten/ten7.ygo', 4978),
+                ('./five/five3.ygo', 4972),
+                ('./eight/eight3.ygo', 4938),
+                ('./eleven/eleven5.ygo', 4922),
+                ('./six/six3.ygo', 4861),
+                ('./four/four2.ygo', 4811),
+                ('./five/five2.ygo', 4805),
+                ('./nine/nine8.ygo', 4755),
+                ('./three/three5.ygo', 4752),
+                ('./seven/seven3.ygo', 4724),
+                ('./ten/ten1.ygo', 4710),
+                ('./twelve/twelve1.ygo', 4675),
+                ('./eight/eight7.ygo', 4668),
+                ('./seven/seven5.ygo', 4664),
+                ('./eleven/eleven1.ygo', 4641),
+                ('./seven/seven7.ygo', 4624),
+                ('./seven/seven8.ygo', 4612),
+                ('./nine/nine4.ygo', 4606),
+                ('./seven/seven1.ygo', 4594),
+                ('./ten/ten3.ygo', 4525),
+                ('./three/three2.ygo', 4515),
+                ('./twelve/twelve3.ygo', 4506),
+                ('./four/four1.ygo', 4502),
+                ('./four/four5.ygo', 4497),
+                ('./two/two5.ygo', 4494),
+                ('./five/five8.ygo', 4433),
+                ('./nine/nine7.ygo', 4418),
+                ('./six/six5.ygo', 4417),
+                ('./five/five1.ygo', 4413),
+                ('./eight/eight8.ygo', 4376),
+                ('./ten/ten6.ygo', 4367),
+                ('./nine/nine6.ygo', 4281),
+                ('./three/three3.ygo', 4278),
+                ('./three/three7.ygo', 4253),
+                ('./five/five7.ygo', 4252),
+                ('./one/one2.ygo', 4243),
+                ('./eight/eight4.ygo', 4230),
+                ('./six/six7.ygo', 4195),
+                ('./two/two2.ygo', 4135),
+                ('./eight/eight6.ygo', 4106),
+                ('./six/six6.ygo', 4069),
+                ('./four/four3.ygo', 4059),
+                ('./eleven/eleven4.ygo', 4047),
+                ('./four/four8.ygo', 4037),
+                ('./six/six1.ygo', 4033),
+                ('./three/three8.ygo', 4020),
+                ('./seven/seven4.ygo', 4009),
+                ('./twelve/twelve2.ygo', 3999),
+                ('./four/four7.ygo', 3941),
+                ('./five/five6.ygo', 3897),
+                ('./six/six8.ygo', 3863),
+                ('./four/four4.ygo', 3811),
+                ('./five/five4.ygo', 3760),
+                ('./three/three1.ygo', 3690),
+                ('./seven/seven6.ygo', 3635),
+                ('./four/four6.ygo', 3509),
+                ('./two/two1.ygo', 3498),
+                ('./six/six4.ygo', 3458),
+                ('./two/two7.ygo', 3405),
+                ('./one/one1.ygo', 3383),
+                ('./two/two3.ygo', 3382),
+                ('./three/three4.ygo', 3372),
+                ('./one/one7.ygo', 3362),
+                ('./one/one5.ygo', 3254),
+                ('./two/two8.ygo', 3253),
+                ('./three/three6.ygo', 3233),
+                ('./one/one8.ygo', 3138),
+                ('./one/one6.ygo', 2980),
+                ('./two/two6.ygo', 2971),
+                ('./one/one3.ygo', 2950),
+                ('./two/two4.ygo', 2936), 
+                ('./one/one4.ygo', 2534)]
+                
 
 COMBO_POINTS = 9
 WATERFRONT_POINTS = 4.5
@@ -510,14 +590,18 @@ def run_simulation(deckFileName):
     return score
 
 def calculate_confidence_interval(data, alpha):
-    if alpha == .01 and NUM_SAMPLE_MEANS == 101:
+    if alpha == .01 and NUM_SAMPLE_MEANS == 201:
+        tVal = 2.601
+    elif alpha == .01 and NUM_SAMPLE_MEANS == 101:
         tVal = 2.626
-    if alpha == .01 and NUM_SAMPLE_MEANS == 51:
+    elif alpha == .01 and NUM_SAMPLE_MEANS == 51:
         tVal = 2.678
     elif alpha == .01 and NUM_SAMPLE_MEANS == 26:
         tVal = 2.787
     elif alpha == .01 and NUM_SAMPLE_MEANS == 10:
         tVal = 3.169
+    elif alpha == .1 and NUM_SAMPLE_MEANS == 101:
+        tVal = 1.660 
     
     avg = sum(data)/len(data)
     sqrdError = 0
@@ -533,68 +617,59 @@ def calculate_confidence_interval(data, alpha):
 def main():
     global NUM_HANDS
     global NUM_SAMPLE_MEANS
+
     start = time.time()
-    if len(sys.argv) == 1:
-        run_simulation(DECK_FILE_NAME)
+
+    if sys.argv[1] == 'file':
+        if len(sys.argv) == 2: #default file/params
+            run_simulation(DECK_FILE_NAME)
+        elif len(sys.argv) == 3: #custom file/default params
+            run_simulation(sys.argv[2])
+        elif len(sys.argv) == 4: #custom file/custom params
+            NUM_HANDS = int(sys.argv[3])
+            run_simulation(sys.argv[2])
 
 
-    elif len(sys.argv) == 2:
+    elif sys.argv[1] == 'optimize-winners':
+        for i in range(100):
+            means = []
+            for filename, score in ALL_DECK_NAMES:
+                scoreList = []
+                for i in range(NUM_SAMPLE_MEANS):
+                    scoreList.append(run_simulation(filename))
+                mean, lowerBound, upperBound = calculate_confidence_interval(scoreList, .01)
+                means.append((filename, mean))
+
+            means = sorted(means, key = lambda x : x[1])
+            for i in range(len(means)):
+                winners[means[i][0]] += i
+
+        print(sorted(winners.items(), key = lambda x : -x[1]))
+    
+    elif sys.argv[1] == 'optimize-two':
+        deck1 = './ten/ten4.ygo'
+        deck2 = './nine/nine5.ygo'
         winners = defaultdict(int)
-        if sys.argv[1] == 'optimize-winners':
-            savefile = 'optimize.txt'
+        deck1Scores = []
+        deck2Scores = []
 
-            for i in range(100):
-                means = []
-                for filename, score in firstRoundWinners:
-                    scoreList = []
-                    for i in range(NUM_SAMPLE_MEANS):
-                        scoreList.append(run_simulation(filename))
-                    mean, lowerBound, upperBound = calculate_confidence_interval(scoreList, .01)
-                    means.append((filename, mean))
+        for i in range(NUM_SAMPLE_MEANS):
+            deck1Scores.append(run_simulation(deck1))
 
-                means = sorted(means, key = lambda x : x[1])
-                for i in range(len(means)):
-                    winners[means[i][0]] += i
+        for i in range(NUM_SAMPLE_MEANS):
+            deck2Scores.append(run_simulation(deck2))
 
-            print(sorted(winners.items(), key = lambda x : -x[1]))
-        
-        elif sys.argv[1] == 'optimize all':
-            winners = defaultdict(int)
-            savefile = 'quick-optimize.txt'
-            for i in range(100):
-                means = []
-                with open(savefile, 'w') as f:
-                    for k,v in deck_dirs.items():
-                        for i in range(1, v+1):
-                            scoreList = []
-                            filename = f'./{k}/{k}{i}.ygo'
-                            for i in range(NUM_SAMPLE_MEANS):
-                                scoreList.append(run_simulation(filename))
-                            mean, lowerBound, upperBound = calculate_confidence_interval(scoreList, .01)
-                            means.append((filename, mean))
+        mean1, lowerBound1, upperBound1 = calculate_confidence_interval(deck1Scores, .1)
+        mean2, lowerBound2, upperBound2 = calculate_confidence_interval(deck2Scores, .1)
 
-                     
-                    means = sorted(means, key = lambda x : x[1])
-                    for i in range(len(means)):
-                        winners[means[i][0]] += i
+        print('Mean for', deck1, 'was', mean1, 'lower bound', lowerBound1, 'upper bound', upperBound1)
+        print('Mean for', deck2, 'was', mean2, 'lower bound', lowerBound2, 'upper bound', upperBound2)
 
-            print(sorted(winners.items(), key = lambda x : -x[1]))
-
-
-    elif len(sys.argv) == 3:
-        max = 0
-        with open(f'results{sys.argv[1]}.txt', 'w') as f:
-            for i in range(1, int(sys.argv[2]) + 1):
-                filename = f'./{sys.argv[1]}/{sys.argv[1]}{i}.ygo'
-                result = run_simulation(filename)
-                if result > max:
-                    max = result                    
-                    winningDeck = filename
-                f.write(f'{filename} scored {result}\n')
-            f.write(f'{winningDeck} had the highest score with {max}')
-
+        print('Separation was', lowerBound1 - upperBound2)
+                
+            
     end = time.time()
-    print('it took', start-end)
+    print('Your simulation took', end-start, 'seconds')
             
 
 
